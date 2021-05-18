00971a74626cf449e56087922048dbec22a8d8ec
Emmanuel Okafor <54027226+Manny2x@users.noreply.github.com>
2021-05-18 21:28:22 +0100

    Update MainBot.java

--- src/main/java/main/MainBot.java
+++ src/main/java/main/MainBot.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.discordbots.api.client.DiscordBotListAPI;
 
 import java.util.*;
+// Kevins SorinoRPG branch
 
 /**
  * <h1>MainBot</h1>
